56bf2aa9af8ac9dd0b7635642c9471b63b441180
Carlosdvp <carlosdvp.tx@gmail.com>
2023-09-06 19:44:13 -0400

    cleaned up the project, removed boilerplate code and added Tailwind.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-red-600 text-3xl'>Uniswap</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
