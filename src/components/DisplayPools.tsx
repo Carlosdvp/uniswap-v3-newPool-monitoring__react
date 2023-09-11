3daea5953eb0f8120f15603707eaf07f72a81103
Carlosdvp <carlosdvp.tx@gmail.com>
2023-09-11 17:22:28 -0400

    finished styling the Find Pools page, it's looking really nice.

--- src/components/DisplayPools.tsx
+++ src/components/DisplayPools.tsx
@@ -22,6 +22,7 @@ const provider = new ethers.JsonRpcProvider(ETH_MAINNET);
 const uniswapContact = new ethers.BaseContract(uniswapFactoryAddress, factoryAbi, provider);
 
 const DisplayPools = () => {
+  const [currentBlock, setCurrentBlock] = useState<number>(0);
   const [startIndex, setStartIndex] = useState<number>(0);
   const [poolEventData, setPoolEventData] = useState<EventDetails[]>([]);
   const [resetCount, setResetCount] = useState<boolean>(false);
@@ -30,8 +31,29 @@ const DisplayPools = () => {
   const poolData = useSelector((state: RootState) => state.poolData.data);
   const fromBlock = useSelector((state: RootState) => state.poolData.fromBlock);
 
-  const handleBlockNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const blockNumber = Number(e.target.value);
+  const getCurrentBlock = async (): Promise<void> => {
+    try {
+      const currentBlock: number = await provider.getBlockNumber();
+      setCurrentBlock(currentBlock);
+    } catch (error) {
+      console.error('Could not get current block number', error)
+    }
+  }
+  // get latest block number when component is loaded
+  useEffect(() => {
+    getCurrentBlock();
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(getCurrentBlock, 30000);
+    // Clean up the interval when the component unmounts.
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const handleBlockNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const blockNumber = Number(event.target.value);
     dispatch(setFromBlock(blockNumber));
   };
 
@@ -81,15 +103,17 @@ const DisplayPools = () => {
 
   function displayPools(data: EventDetails[], startIndex: number) {
     return data.slice(startIndex, startIndex + 10).map((pool, index) => (
-      <div key={index} className='flex'>
-        <span className='mr-4 p-1'>
+      <div 
+        key={index} 
+        className='flex bg-blue-900 text-white my-1 justify-between'>
+        <span className='py-1 pl-4'>
           Pool Address {startIndex + index + 1}:
         </span>
         <a
           href={`https://etherscan.io/address/${pool.poolAddress}`}
           target="_blank"
           rel="noopener noreferrer"
-          className='hover:text-blue-400 w-[70%] text-right'>
+          className='hover:text-blue-400 pr-4 py-1'>
           {pool.poolAddress}
         </a>
       </div>
@@ -117,11 +141,11 @@ const DisplayPools = () => {
       <Header />
 
       <div>
-        <h1 className='text-red-900 font-semibold text-3xl text-center py-12'>
+        <h1 className='text-orange-700 font-semibold text-3xl text-center py-8'>
           Recently Created Pools on Uniswap V3
         </h1>
 
-        <div className="flex flex-col items-center text-center w-[50%] min-w-[460px] bg-blue-900 text-white h-[20vh] max-h-[280px] min-h-[180px] justify-center mx-auto my-0">
+        <div className="flex flex-col items-center text-center w-[60%] min-w-[460px] bg-blue-900 text-white h-[25vh] max-h-[380px] min-h-[180px] justify-center mx-auto my-0">
           <p className='pb-4'>
             To fetch the Recent Pools Created, enter a value for the <i>fromBlock</i>
           </p>
@@ -137,10 +161,17 @@ const DisplayPools = () => {
             className="bg-blue-400 text-white border border-blue-300 px-6 py-2 hover:bg-white hover:text-black mb-4">
             Get Data
           </button>
+
+          <p className='py-1'>
+            You can use the current block as reference:
+          </p>
+          <p className='text-2xl'>
+            {currentBlock !== null ? currentBlock : 'Loading...'}
+          </p>
         </div>
 
         <div className='text-center'>
-          <div className='py-6 pl-4 mx-auto my-0 mt-10 bg-blue-200 w-[70%]'>
+          <div className='py-6 pl-4 mx-auto my-2 bg-blue-200 w-[60%] min-w-[500px]'>
             <p className='pb-2'>
               Number of Pools Created since block <strong>{fromBlock} :</strong> 
               <span className='text-blue-800 font-bold pl-6 text-xl'>{poolEventData.length}</span>
@@ -149,12 +180,12 @@ const DisplayPools = () => {
               Clicking on a Pool Address will take you to the Etherscan page for that contract.
             </p>
           </div>
-          <div className='py-6 pl-12 w-[60%] min-w-[600px] mx-auto my-0 justify-items-center'>
+          <div className='w-[60%] min-w-[600px] mx-auto my-0'>
             {displayPools(poolEventData, startIndex)}
           </div>
         </div>
 
-        <div className='flex flex-row w-[50%] min-w-[500px] items-center py-6 mx-auto my-0 border-2 border-blue-600 bg-blue-900 text-white'>
+        <div className='flex flex-row w-[60%] min-w-[500px] items-center py-6 mx-auto my-0 border-2 border-blue-600 bg-blue-900 text-white'>
           <div className='flex flex-col w-[50%] items-center'>
             <label className='mb-2'>
               Show next 10 Pools
