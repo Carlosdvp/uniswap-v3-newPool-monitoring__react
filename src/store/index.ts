54f5e5b2fdaa56f1ee23ea94f9aebb3f54567c0b
Carlosdvp <carlosdvp.tx@gmail.com>
2023-09-26 19:59:01 -0400

    creatd a new slice for the newPoolData in the store, added one reducer, upated redux store configuration file with the new slice.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,17 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit"
 import poolDataSlice from "./poolDataSlice"
+import newPoolDataSlice from "./newPoolDataSlice"
 
 export const store = configureStore({
   reducer: {
-    poolData: poolDataSlice
+    poolData: poolDataSlice,
+    newPoolData: newPoolDataSlice,
   },
 })
 
 const rootReducer = combineReducers({
   poolData: poolDataSlice,
+  newPoolData: newPoolDataSlice,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
